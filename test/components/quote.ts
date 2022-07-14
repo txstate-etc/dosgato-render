91cbc15d69a4478f66f5d16e0a0f808cb40e401c
Nick Wing <nwing@txstate.edu>
2022-07-13 22:25:26 -0500

    flesh out the "quote" test template

--- test/components/quote.ts
+++ test/components/quote.ts
@@ -1,22 +1,23 @@
 import { Component, ComponentData } from '@dosgato/templating'
 import { htmlEncode } from 'txstate-utils'
 
-export interface RichTextData extends ComponentData {
-  title: string
-  text: string
+export interface QuoteData extends ComponentData {
+  author: string
+  quote: string
 }
 
-export class QuoteTemplate extends Component<RichTextData> {
+export class QuoteTemplate extends Component<QuoteData> {
   static templateKey = 'keyc3'
   static cssBlocks = new Map([
     ['quote', {
       css: `
-        .dg-rich-text img { max-width: 100%; }
+        .dg-quote { margin: 1em; padding: 1em; background-color: #DDDDDD; border: 1px solid #CCCCCC; }
+        .dg-author { display: block; text-align: right; font-weight: bold; }
       `
     }]
   ])
 
   render (renderedAreas: Map<string, string[]>) {
-    return `${this.editBar()}${this.data.title ? '<h2>' + htmlEncode(this.data.title) + '</h2>' : ''}<div class="dg-rich-text">${this.data.text}</div>`
+    return `${this.editBar()}<div class="dg-quote">${htmlEncode(this.data.quote)}<span class="dg-author">- ${htmlEncode(this.data.author)}</span></div>`
   }
 }
